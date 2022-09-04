79f2a49ee4b525a047102651205ac80282f63205
alvo254 <alvinkhamron@gmail.com>
2022-09-04 19:32:47 +0300

    fixes

---
+++ db/heroku_seeds.rb
@@ -0,0 +1,3 @@
+ENV["DATABASE_URL"] = "postgres://wsjsvxslgactxz:dec774d7c4425d1868981cdac461719b4fd988c30b7bff513bbd2ef418bc6638@ec2-18-208-55-135.compute-1.amazonaws.com:5432/d1jc5k4vceg426"
+
+require_relative('./seeds.rb')
