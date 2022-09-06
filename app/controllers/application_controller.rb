f1de55b423cea8935d14c78aff9524bd975ff751
alvo254 <alvinkhamron@gmail.con>
2022-09-06 09:42:33 +0300

    deleting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,59 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  get '/reviews' do
-    restaurant = Restaurant.all
-      restaurant.to_json(include: {reviews: {only: [:body], include: {user: {only: [:name]}}}})
-    end
-
-    post '/reviewss' do
-      review = Review.create(
-        body: params[:body],
-        restaurant_id: params[:restaurant_id],
-        user_id: params[:user_id]
-      )
-      review.to_json
-    end
-
-    get '/reviewss' do
-      reviewss = Review.all
-      reviewss.to_json
-
-    end
-
-    get '/restaurants' do 
+    get '/reviews' do
       restaurant = Restaurant.all
+        restaurant.to_json(include: {reviews: {only: [:body], include: {user: {only: [:name]}}}})
+      end
+
+      post '/reviewss' do
+        review = Review.create(
+          body: params[:body],
+          restaurant_id: params[:restaurant_id],
+          user_id: params[:user_id]
+        )
+        review.to_json
+      end
+
+      get '/reviewss' do
+        reviewss = Review.all
+        reviewss.to_json
+
+      end
+
+      get '/users' do
+        users = User.all
+        users.to_json
+      end
+
+      # resutrants routes
+
+      get '/restaurants' do 
+        restaurant = Restaurant.all
+        restaurant.to_json
+      end
+
+      post '/restaurants' do
+        restaurant = Restaurant.create(
+          name: params[:name],
+          location: params[:location]
+        )
+        restaurant.to_json
+    end
+    
+    delete '/restaurants/:id' do 
+      restaurant = Restaurant.find(params[:id])
+      restaurant.destroy
       restaurant.to_json
     end
 
-    post '/restaurants' do
-      restaurant = Restaurant.create(
+    patch '/restaurants/:id' do 
+      restaurant = Restaurant.find(params[:id])
+       restaurant.update(
         name: params[:name],
         location: params[:location]
       )
       restaurant.to_json
-  end
-
-    get '/users' do
-      users = User.all
-      users.to_json
     end
+
 end
